698335c56a07b05102f275015a20cbdb2a54237b
Leandro Russo <sc21l2r@leeds.ac.uk>
2024-03-20 10:25:11 +0000

    client: started the login and set up the prompt main menu

--- myclient/client.py
+++ myclient/client.py
@@ -105,6 +105,7 @@ class ClientNews():
     def __init__(self):
         #initialise session
         session = requests.Session()
+        self.available_url = []
         
         #get the list of all agencies
         response = session.get(URL_LIST)
@@ -112,10 +113,22 @@ class ClientNews():
             agencies = response.json()
             if isinstance(agencies, list):
                 self.agencies = agencies
+                for agency in self.agencies:
+                    self.available_url.append(agency["url"])
+
             else:
                 raise AssertionError("Expected a list when receiving data but it's not")
         else:
             raise RuntimeError("Could not get any data from the API. Try later")
+    
+    def login(self, possible_url: str) -> str:
+        #log-in into a news agency
+        #TODO: can I login in more than one agency at the time
+        if possible_url in self.available_url:
+            username = input("Username: ")
+            password = input("Password: ")
+        
+
     
     def list(self):
         table_data = []
@@ -135,11 +148,85 @@ class ClientNews():
 
         # Print the tabulated data
         print(tabulate(table_data, headers=["Agency Name", "URL", "Agency Code"], tablefmt="grid"))
+    
+    def print_commands(self):
+        print("Available commands:")
+        print("-" * 40)
+        
+        commands = [
+            ("login url", "Log-in into a specific news agency. Substitute url with the URL of the service."),
+            ("logout", "Log out from the current session."),
+            ("post", "Post a new story."),
+            ("news -id= -cat= -reg= -date=", "Request news stories from one or multiple news agencies."),
+            ("flags", "Print out more details about news flags."),
+            ("list", "List all the agencies stored in the main directory."),
+            ("delete key", "Delete news story. Substitute key with the story key."),
+            ("quit", "Quit the program.")
+        ]
+        
+        for command, description in commands:
+            print(f"{command:<30} {description}")
 
 
 
 
 client = ClientNews()
 
+client.print_commands()
+while True:
 
-client.list()
+    user_prompt = input("Insert command: ")
+
+    if "login " in user_prompt:
+        words = user_prompt.split()
+
+        if len(words) == 2:
+            if words[0] == "login":
+                if "pythonanywhere.com" in words[1]:
+                    answer = login(words[1])
+                    print(answer)
+                else:
+                    print("Only allowed to use url from pythonanywhere.com for this coursework")
+            else:
+                print("bad argument when checking login. try again. e.g. login sre31r.pythonanywhere.com")
+        else:
+            print("login bad argument, try again. e.g. login sre31r.pythonanywhere.com")
+
+
+        
+    elif user_prompt == "logout":
+        print("in logout")
+        
+    elif user_prompt == "post":
+        print("in post")
+        
+    elif "news " in user_prompt:
+        print("in news")
+        
+    elif user_prompt == "list":
+        client.list()
+
+    elif "delete " in user_prompt:
+        print("in delete")
+
+    elif user_prompt == "flags":
+        print()
+        print("All flags are optional")
+        print("(id flag) id is the id of the news agency e.g. -id='JAD05'")
+        print("(cat flag) cat is the news category. if specified e.g. -cat='tech' returns results from that category")
+        print("(reg flag) the region of the required stories, e.g -reg='uk'")
+        print("(date flag) the date in dd/mm/yyyy after which a story has been added. e.g. -date='12/2/2019'")
+        print()
+    elif user_prompt == "show":
+        client.print_commands()
+
+    elif user_prompt == "quit":
+        break
+
+    else:
+        print("invalid command, try again")
+    
+    print()
+    print("use command 'show' to show all the available commands")
+
+    
